1125841eb3f4dd8c483f631e6a90a483f5fc2567
Stanislav Yanakiev <stanislav.yanakievv@gmail.com>
2022-07-21 11:55:16 +0300

    Add files via upload

---
+++ animals/testcode.py
@@ -0,0 +1,21 @@
+from animals.cat import Cat
+from animals.dog import Dog
+from animals.kitten import Kitten
+from animals.tomcat import Tomcat
+
+dog = Dog("Rocky", 3, "Male")
+print(dog.make_sound())
+print(dog)
+tomcat = Tomcat("Tom", 6)
+print(tomcat.make_sound())
+print(tomcat)
+
+
+
+kitten = Kitten("Kiki", 1)
+print(kitten.make_sound())
+print(kitten)
+cat = Cat("Johnny", 7, "Male")
+print(cat.make_sound())
+print(cat)
+
